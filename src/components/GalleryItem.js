29ab370ca79b3f9d984a481931259825a11c5ca7
okayzach <znortyy@gmail.com>
2022-06-27 13:40:12 -0400

    added react router DOM and finished hw

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -35,7 +35,16 @@ const GalleryItem = (props) => {
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/artist/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
